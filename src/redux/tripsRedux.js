e59f4697c6a6ae591a30f2cb646e43b8c7f88349
michal1986-web <57727656+michal1986-web@users.noreply.github.com>
2020-02-02 14:30:52 +0100

    Show views

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -21,18 +21,18 @@ export const getFilteredTrips = ({trips, filters}) => {
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+  const filtered = trips.filter(trip => trip.id == tripId);
 
-  // TODO - filter trips by tripId
+  // DONE - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
 
-  // TODO - filter trips by countryCode
+  // DONE - filter trips by countryCode
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
