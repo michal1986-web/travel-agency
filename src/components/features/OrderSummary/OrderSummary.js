a54bd506782ad61dcfba5217c2b2e44f5364cff4
michal1986-web <57727656+michal1986-web@users.noreply.github.com>
2020-02-05 15:15:39 +0100

    Add travel price v0.2

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './OrderSummary.scss';
+import PropTypes from 'prop-types';
+
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+
+const OrderSummary = ({cost, options}) => (
+  <h2 className={styles.component}>
+    Total:
+    <strong>
+    ${calculateTotal(formatPrice(cost), options)}
+    </strong>
+  </h2>
+);
+
+OrderSummary.propTypes = {
+  cost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary; 
